20be1bc01eead1d9672f73776ad6ec9f28349fc5
MichaelCodeAndre <michae0101@osloskolen.no>
2021-10-17 07:45:33 +0200

    Adding more stuff to test

--- Projects/Other/Random quote generator/script/script.js
+++ Projects/Other/Random quote generator/script/script.js
@@ -1,9 +1,46 @@
-var quotes = ['This is the first quote.',
-'This is the second quote.', 
-'This is the third quote.', 
-'I wanna do more coding it\'s so cool.']
+var quotes = [
+'Quote 1',
+'Quote 2',
+'Quote 3',
+'Quote 4',
+'Quote 5',
+'Quote 6',
+'Quote 7',
+'Quote 8',
+'Quote 9',
+'Quote 10'
+]
+var voiceQuotes = [
+    'sound1',
+    'sound2',
+    'sound3',
+    'sound4',
+    'sound5',
+    'sound6',
+    'sound7',
+    'sound8',
+    'sound9',
+    'sound10'
+]
+
+//Fetched from the internet, used for playing/pausing.
+var myAudio = document.getElementById('myAudio');
+var isPlaying = false;
+
+function togglePlay() {
+    isPlaying ? myAudio.pause() : myAudio.play();
+};
+
+myAudio.onplaying = function() {
+    isPlaying = true;
+};
+
+myAudio.onpause = function() {
+    isPlaying = flase;
+};
+//end of fetched from internet.
 
 function newQuote() {
-    var randomNumber = Math.floor(Math.random() *(quotes.lenght));
+    var randomNumber = Math.floor(Math.random() * (quotes.length));
     document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
 }
